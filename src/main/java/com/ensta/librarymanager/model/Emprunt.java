e00df51fcd7973b675c10f892946915b5946b62e
MarinENSTA <marin.stamm@ensta-paris.fr>
2021-04-01 20:38:00 +0200

    Exercice 2 + testModel, add des fichier oubliés.

---
+++ src/main/java/com/ensta/librarymanager/model/Emprunt.java
@@ -0,0 +1,70 @@
+package com.ensta.librarymanager.model;
+import java.time.LocalDate;
+
+public class Emprunt
+{
+	private int id;
+	private Membre idMembre;
+	private Livre idLivre;
+	private LocalDate dateEmprunt;
+	private LocalDate dateRetour;
+
+	public Emprunt(int id, Membre idMembre, Livre idLivre, LocalDate dateEmprunt, LocalDate dateRetour) {
+		this.id = id;
+		this.idMembre = idMembre;
+		this.idLivre = idLivre;
+		this.dateEmprunt = dateEmprunt;
+		this.dateRetour = dateRetour;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Membre getIdMembre() {
+		return idMembre;
+	}
+
+	public void setIdMembre(Membre idMembre) {
+		this.idMembre = idMembre;
+	}
+
+	public Livre getIdLivre() {
+		return idLivre;
+	}
+
+	public void setIdLivre(Livre idLivre) {
+		this.idLivre = idLivre;
+	}
+
+	public LocalDate getDateEmprunt() {
+		return dateEmprunt;
+	}
+
+	public void setDateEmprunt(LocalDate dateEmprunt) {
+		this.dateEmprunt = dateEmprunt;
+	}
+
+	public LocalDate getDateRetour() {
+		return dateRetour;
+	}
+
+	public void setDateRetour(LocalDate dateRetour) {
+		this.dateRetour = dateRetour;
+	}
+
+	@Override
+	public String toString() {
+		return "Emprunt{" +
+				"id=" + id +
+				", idMembre= (" + idMembre +
+				"), idLivre= (" + idLivre +
+				"), dateEmprunt=" + dateEmprunt +
+				", dateRetour=" + dateRetour +
+				'}';
+	}
+}
--- src/test/java/ensta/ModeleTest.java
+++ src/test/java/ensta/ModeleTest.java
@@ -10,39 +10,38 @@ import java.time.LocalDate;
 import static org.junit.Assert.*;
 
 public class ModeleTest {
+    Membre TestMembre = new Membre(0, "STAMM", "Marin", "Ensta Paris", "marin.stamm@ensta-paris.fr", "+33615269117", Membre.Abonnement.VIP );
+    Livre TestLivre = new Livre(0, "Boule de Foudre", "Liu Cixin", "12345");
+    Emprunt TestEmprunt = new Emprunt(0,TestMembre, TestLivre, LocalDate.now(), LocalDate.now());
 
     @Test
     public void TestEmprunt0()
     {
-        Emprunt testEmprunt = new Emprunt(0,0, 42, LocalDate.now(), LocalDate.now());
-        assertEquals(0, testEmprunt.getId());
+        assertEquals(0, TestEmprunt.getId());
     }
 
     @Test
     public void TestEmprunt1()
     {
-        Emprunt testEmprunt = new Emprunt(0,0, 42, LocalDate.now(), LocalDate.now());
-        assertEquals(42, testEmprunt.getIdLivre());
+        assertNotEquals(42, TestEmprunt.getIdLivre());
+        System.out.println(TestEmprunt.toString());
     }
 
     @Test
     public void TestMembre0()
     {
-        Membre TestMembre = new Membre(0, "STAMM", "Marin", "Ensta Paris", "marin.stamm@ensta-paris.fr", "+33615269117", Membre.Abonnement.VIP );
         assertEquals(Membre.Abonnement.VIP, TestMembre.getAbonnement());
     }
 
     @Test
     public void TestMembre1()
     {
-        Membre TestMembre = new Membre(0, "STAMM", "Marin", "Ensta Paris", "marin.stamm@ensta-paris.fr", "+33615269117", Membre.Abonnement.VIP );
         assertNotEquals(TestMembre.getAdresse(), "Ensta Bretagne");
     }
 
     @Test
     public void TestLivre0()
     {
-        Livre TestLivre = new Livre(0, "Boule de Foudre", "Liu Cixin", "12345");
         assertNotEquals("Léonard de Vinci", TestLivre.getAuteur());
     }
 }
